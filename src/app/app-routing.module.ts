d66d24e4b2ef8ad0617b387e6743e71aace470ea
HimanshuChelani27 <himanshuchelani4@gmail.com>
2024-08-10 19:05:14 -0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { combineLatest } from 'rxjs';
+import { CategoryComponent } from './category/category.component';
+import { CategoryDetailsComponent } from './category-details/category-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:"category", component: CategoryComponent },
+  {path:"category-details", component: CategoryDetailsComponent },
+  {path:"", redirectTo:"category", pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoryDetailsComponent } from './category-details/category-details.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryDetailsComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
